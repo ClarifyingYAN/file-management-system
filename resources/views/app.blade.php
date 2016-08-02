74fd464369da8be1e021d68ccc38f73ebb697cfa
YAN <hu_yancheng@outlook.com>
2016-08-02 13:47:37 +0800

    home and welcome page reset, app.balde.php add files adn settings btn

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,11 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
+	<title>file-management-system</title>
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+	{{--new add--}}
+	<link href="{{ asset('/css/table.css') }}" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
@@ -28,12 +30,14 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Laravel</a>
+				<a class="navbar-brand" href="#">File-management-system</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
 					<li><a href="{{ url('/') }}">Home</a></li>
+					<li><a href="{{ url('/file') }}">Files</a></li>
+					<li><a href="{{ url('/settings') }}">Settings</a></li>
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
