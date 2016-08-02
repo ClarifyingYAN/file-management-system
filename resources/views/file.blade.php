40de97ef20a65c9af87691d2ad0a261b4224f109
YAN <hu_yancheng@outlook.com>
2016-08-02 15:54:44 +0800

    change view format

--- resources/views/file.blade.php
+++ resources/views/file.blade.php
@@ -1,34 +1,26 @@
 @extends('app')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-				{{--<div class="panel-body">--}}
-					{{--You are logged in!--}}
-				{{--</div>--}}
-				<div class="show-list">
-					<table class="bordered">
-						<thead>
-							<th>#</th>
-							<th>file name</th>
-							<th>size</th>
-							<th></th>
-						</thead>
-						@foreach($files as $key => $file)
-							<tr>
-								<td>{{ $key }}</td>
-								<td>{{ $file['name'] . $file['ext'] }}</td>
-								<td>{{ $file['size'] }}</td>
-								<td></td>
-							</tr>
-						@endforeach
-					</table>
-				</div>
-			</div>
-		</div>
+	<div class="panel-heading">Home</div>
+	{{--<div class="panel-body">--}}
+	{{--You are logged in!--}}
+	{{--</div>--}}
+	<div class="show-list">
+		<table class="bordered">
+			<thead>
+			<th>#</th>
+			<th>file name</th>
+			<th>size</th>
+			<th></th>
+			</thead>
+			@foreach($files as $key => $file)
+				<tr>
+					<td>{{ $key }}</td>
+					<td>{{ $file['name'] . $file['ext'] }}</td>
+					<td>{{ $file['size'] }}</td>
+					<td></td>
+				</tr>
+			@endforeach
+		</table>
 	</div>
-</div>
 @endsection
--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,28 +1,19 @@
 @extends('app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Home</div>
-                    {{--<div class="panel-body">--}}
-                    {{--You are logged in!--}}
-                    {{--</div>--}}
-                    <div class="settings-table">
-                        <table class="bordered">
-                            <tr>
-                                <td>管理根目录</td>
-                                <form action="{{ url('/settings') }}">
-                                    <td><input type="text" name="root-dir"></td>
-                                    <td><input type="submit" value="apply"></td>
-                                </form>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="panel-heading">Home</div>
+    {{--<div class="panel-body">--}}
+    {{--You are logged in!--}}
+    {{--</div>--}}
+    <div class="settings-table">
+        <table class="bordered">
+            <tr>
+                <td>管理根目录</td>
+                <form action="{{ url('/settings') }}">
+                    <td><input type="text" name="root-dir"></td>
+                    <td><input type="submit" value="apply"></td>
+                </form>
+            </tr>
+        </table>
     </div>
-
 @stop
