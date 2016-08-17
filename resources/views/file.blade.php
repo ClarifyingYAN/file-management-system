dd3f36c9f9bae5f8af2f653e5c318b70977a898d
YAN <hu_yancheng@outlook.com>
2016-08-17 16:26:52 +0800

    add comment, breadcrumb and change css

--- resources/views/file.blade.php
+++ resources/views/file.blade.php
@@ -3,22 +3,39 @@
 @section('content')
 	<div class="panel-heading">
 		<span class="panel-left-name">File</span>
-		<span class="panel-left-btns">
-			{!! Form::open(['action'=>'FileController@upload', 'files'=>true, 'class'=>'form']) !!}
-				{!! Form::file('file') !!}
-				{!! Form::hidden('shortPath', $dir['shortPath']) !!}
-				{!! Form::submit('upload', ['class'=>'btns']) !!}
-			{!! Form::close() !!}
-			{!! Form::open(['action'=>'FileController@makeFolder', 'class'=>'form']) !!}
-				{!! Form::text('folderName') !!}
-				{!! Form::hidden('shortPath', $dir['shortPath']) !!}
-				{!! Form::submit('New Folder', ['class'=>'btns']) !!}
-			{!! Form::close() !!}
-		</span>
 	</div>
-	{{--<div class="panel-body">--}}
-	{{--You are logged in!--}}
-	{{--</div>--}}
+	<div>
+		<!-- upload form -->
+		{!! Form::open(['action'=>'FileController@upload', 'files'=>true, 'class'=>'form-inline', 'id'=>'upload-form']) !!}
+			<div class="form-group">
+				{!! Form::file('file', ['class'=>'exampleInputFile']) !!}
+				{!! Form::hidden('shortPath', $dir['shortPath']) !!}
+			</div>
+		{!! Form::submit('upload', ['class'=>'btn btn-default']) !!}
+		{!! Form::close() !!}
+		<!-- make folder form -->
+		{!! Form::open(['action'=>'FileController@makeFolder', 'class'=>'form-inline']) !!}
+			{!! Form::text('folderName', '', ['class'=>'form-control', 'placeholder'=>'folder name', 'id'=>'exampleInputName2']) !!}
+			{!! Form::hidden('shortPath', $dir['shortPath']) !!}
+			{!! Form::submit('New Folder', ['class'=>'btn btn-default']) !!}
+		{!! Form::close() !!}
+	</div>
+	<!-- breadcrumb -->
+	<div>
+		<ol class="breadcrumb">
+				<li><a href="{{ URL::action('FileController@index') }}">/</a></li>
+			<?php
+				$arr = '';
+			?>
+			@for($i = 1; $i < count($dir['shortPathArr']); $i++)
+				<?php
+					$shortPath = breadcrumbPath($dir['shortPathArr'], $i);
+				?>
+				<li><a href="{{ URL::action('FileController@index', ['folder'=>$shortPath]) }}">{{ $dir['shortPathArr'][$i] }}</a></li>
+			@endfor
+		</ol>
+	</div>
+	<!-- dir list -->
 	<div class="show-list">
 		<table class="bordered">
 			<thead>
@@ -28,6 +45,7 @@
 			<th>type</th>
 			<th>operate</th>
 			</thead>
+			{{-- files list --}}
 			@foreach($dir['files'] as $key => $file)
 				<tr>
 					<td>{{ $key }}</td>
@@ -41,35 +59,41 @@
 						@endif
 					</td>
 					<td>
-						{!! Form::open(['url'=>'/file/deleteFile', 'method'=>'delete','class'=>'form']) !!}
-							{!! Form::hidden('fileName', $file['pathName']) !!}
-							{!! Form::submit('Del', ['class'=>'btns']) !!}
-						{!! Form::close() !!}
 						{!! Form::open(['url'=>'/file/download', 'method'=>'post', 'class'=>'form']) !!}
 							{!! Form::hidden('pathName', $file['pathName']) !!}
-							{!! Form::submit('Download', ['class'=>'btns']) !!}
+							{!! Form::submit('Download', ['class'=>'btn btn-primary']) !!}
 						{!! Form::close() !!}
-{{--						<a href="{{ URL::action('FileController@download', ['pathName'=>$file['pathName']]) }}">download</a>--}}
+						{!! Form::open(['url'=>'/file/deleteFile', 'method'=>'delete','class'=>'form']) !!}
+							{!! Form::hidden('fileName', $file['pathName']) !!}
+							{!! Form::submit('Delete', ['class'=>'btn btn-danger']) !!}
+						{!! Form::close() !!}
 					</td>
 				</tr>
 			@endforeach
+			{{-- folders list --}}
 			@foreach($dir['folders'] as $key => $folder)
 				<tr>
 					<td>{{ $key }}</td>
 					<td><a href="{{ URL::action('FileController@index', ['folder'=>$folder['pathName']]) }}">{{ $folder['name'] }}</a></td>
 					<td>{{ humanSize($folder['size']) }}</td>
-					<td>
-						{{ 'dir' }}
-					</td>
+					<td>dir</td>
 					<td>
 						{!! Form::open(['url'=>'/file/deleteFolder', 'method'=>'delete','class'=>'form']) !!}
 							{!! Form::hidden('folderName', $folder['pathName']) !!}
-							{!! Form::submit('Del', ['class'=>'btns']) !!}
+							{!! Form::submit('Delete', ['class'=>'btn btn-danger']) !!}
 						{!! Form::close() !!}
-						{{--<a href="{{ URL::action('FileController@download', ['pathName'=>$folder['pathName']]) }}">download</a>--}}
 					</td>
 				</tr>
 			@endforeach
 		</table>
 	</div>
 @endsection
+{{--@section('errors')--}}
+	{{--@if($errors->any())--}}
+		{{--<ul class="list-group">--}}
+			{{--@foreach($errors->all() as $error)--}}
+				{{--<li class="list-group-item list-group-item-danger">{{ $error }}</li>--}}
+			{{--@endforeach--}}
+		{{--</ul>--}}
+	{{--@endif--}}
+{{--@endsection--}}
